258c0718e184962c634c00d83c68d4a593894d37
Yipei Zhao <zhaoyipei99@gmail.com>
2022-06-11 10:48:25 +0100

    comments for construction

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ class notebook:
     # time, python time object. Current time
     # description, str. Description of the algorithm
     # return:
-    # void
+    # bool, successfully added or not
     def create(algo_name, time = datetime.now().strftime("%d/%m/%Y %H:%M:%S"), description):
-        return Null
+
+        return 0
 
     # Edit an algorithm
     # para:
@@ -28,15 +29,31 @@ class notebook:
     # return:
     # Bool, successfully changed or not
     def edit(algo_name,attribute,content):
-        return Null
+        return 0
 
+    # Add example record to an entry
+    # para:
+    # example_jason, json of the example
+    # return:
+    # bool, successful added or not
+    def add_example(example_json):
+        return 0
+
+    # Add a function to exsist algorithm
+    # para:
+    # algo_name, str. Name of the algorithm
+    # file_path, str. Directory of the file
+    # return:
+    # bool, wether the function is added or not
+    def add_function(alg_name,file_path):
+        return 0
     # Delete an algorithm
     # para:
     # algo_name, str. Algorithm name
     # return:
     # Bool, Successfully deleted or not
     def delete(algo_name):
-        return Null
+        return 0
 
     # Serach an algorithm
     # para:
@@ -46,4 +63,10 @@ class notebook:
     def search(algo_name):
         return 0
 
+    # Display info
+    # para: algo_name,str
+    # return:
+    # void
+    def display(algo,name):
+        return 0
 
